5d03f05e154852181ec01c6d8a30eebb1092bbf9
TropinDmitry <85580920+TropinDmitry@users.noreply.github.com>
2022-06-07 23:37:31 +0700

    Initial commit

---
+++ lab13_IM/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Sale = new System.Windows.Forms.Button();
+            this.Buy = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edRate = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.Sale);
+            this.panel1.Controls.Add(this.Buy);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btStart);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.edRate);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(717, 82);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(407, 56);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(213, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Sale
+            // 
+            this.Sale.Location = new System.Drawing.Point(553, 18);
+            this.Sale.Name = "Sale";
+            this.Sale.Size = new System.Drawing.Size(67, 32);
+            this.Sale.TabIndex = 9;
+            this.Sale.Text = "Продать";
+            this.Sale.UseVisualStyleBackColor = true;
+            this.Sale.Click += new System.EventHandler(this.Sale_Click);
+            // 
+            // Buy
+            // 
+            this.Buy.Location = new System.Drawing.Point(407, 18);
+            this.Buy.Name = "Buy";
+            this.Buy.Size = new System.Drawing.Size(67, 32);
+            this.Buy.TabIndex = 8;
+            this.Buy.Text = "Купить";
+            this.Buy.UseVisualStyleBackColor = true;
+            this.Buy.Click += new System.EventHandler(this.Buy_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(281, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(105, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Доллары";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(281, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(105, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(73, 47);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(75, 23);
+            this.btStart.TabIndex = 4;
+            this.btStart.Text = "Next day";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(238, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Рубли";
+            // 
+            // edRate
+            // 
+            this.edRate.DecimalPlaces = 2;
+            this.edRate.Location = new System.Drawing.Point(106, 16);
+            this.edRate.Name = "edRate";
+            this.edRate.Size = new System.Drawing.Size(96, 20);
+            this.edRate.TabIndex = 1;
+            this.edRate.Value = new decimal(new int[] {
+            7433,
+            0,
+            0,
+            131072});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Текущий курс";
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart1.Location = new System.Drawing.Point(0, 82);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsValueShownAsLabel = true;
+            series1.LabelFormat = "F2";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(717, 368);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button Sale;
+        private System.Windows.Forms.Button Buy;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.NumericUpDown edRate;
+    }
+}
+
---
+++ lab13_IM/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            rate = (double)edRate.Value;
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(i, rate);
+        }
+
+        const double k = 0.05;
+        int i = 0;
+        double rate;
+        double rubles = 100;
+        double mu = 0.0001, sigma = 0.0084;
+        int dollars = 0;
+        Random random = new Random();
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            rate = (double)edRate.Value;
+
+            i++;
+            rate = rate * Math.Exp((mu - sigma * sigma / (double)2) + sigma * (2 * random.NextDouble() - 1));
+
+            chart1.Series[0].Points.AddXY(i, rate);
+
+            textBox1.Text = rubles.ToString("F2");
+            textBox2.Text = dollars.ToString();
+            textBox3.Text = "          ";
+        }
+
+        private void Buy_Click(object sender, EventArgs e)
+        {
+            if (rubles >= rate) // если достаточно средств
+            {
+                dollars++;
+                rubles -= rate;
+                textBox1.Text = rubles.ToString("F2");
+                textBox2.Text = dollars.ToString();
+                textBox3.Text = "Покупка прошла успешно";
+            }
+            else
+            {
+                textBox3.Text = "Недостаточно средств"; // не можем купить
+            }
+        }
+
+        private void Sale_Click(object sender, EventArgs e)
+        {
+            if (dollars >= 1)
+            {
+                dollars--;
+                rubles += rate;
+                textBox1.Text = rubles.ToString("F2");
+                textBox2.Text = dollars.ToString();
+                textBox3.Text = "Продажа прошла успешно";
+            }
+            else
+            {
+                textBox3.Text = "Недостаточно долларов для продажи"; // не можем продать
+            }
+        }
+
+    }
+}
